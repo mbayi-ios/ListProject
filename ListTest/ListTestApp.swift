e21b029cf331b4fa6af4dda6b4c24b467e8dc6b8
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-10-09 18:58:33 +0300

    Initial Commit

---
+++ ListTest/ListTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListTestApp.swift
+//  ListTest
+//
+//  Created by Amby on 09/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
