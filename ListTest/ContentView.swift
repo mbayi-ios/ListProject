a851743940ca057a50f169e2e0f4e9d7f123afe6
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-10-09 19:26:21 +0300

    all in one source file

--- ListTest/ContentView.swift
+++ ListTest/ContentView.swift
@@ -7,10 +7,48 @@
 
 import SwiftUI
 
+struct Item: Identifiable {
+    var id: UUID
+    var name: String
+
+}
+
 struct ContentView: View {
+    @State var items = [Item(id: UUID(), name: "first"), Item(id: UUID(), name: "second")]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+        List {
+            ForEach(items) { item in
+                NavigationLink(destination: Text("Destination \(item.name)"), label: {
+                    Text(item.name)
+        
+                })
+
+            }
+            .onDelete(perform: { indexSet in
+                for index in indexSet {
+                    items.remove(at: index)
+                }
+            })
+            .onMove(perform: {indices, newOffset in
+                items.move(fromOffsets: indices, toOffset: newOffset)
+            })
+        }
+        .navigationBarTitle(Text("Todo's"), displayMode: .large)
+        .toolbar(content: {
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                EditButton()
+
+                Button(action: {
+                    items.append(Item(id: UUID(), name: "newly added"))
+                }, label: {
+                    Image(systemName: "plus")
+                })
+            }
+        })
+
+        }
     }
 }
 
